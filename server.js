3783a5c18ca140a667c228871d4e0ab43156b93e
Shrija Jha <153726869+shrija-jha@users.noreply.github.com>
2026-06-20 17:50:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,15 +10,12 @@ app.use(express.json());
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: { 
-        // This is the standard path for Chromium on Railway's Linux environment
-        executablePath: '/usr/bin/chromium-browser', 
         args: [
             '--no-sandbox', 
             '--disable-setuid-sandbox',
             '--disable-dev-shm-usage',
-            '--disable-accelerated-2d-canvas',
-            '--no-first-run',
             '--no-zygote',
+            '--single-process',
             '--disable-gpu'
         ]
     }
