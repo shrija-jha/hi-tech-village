0e7a3c1168790c5039d9bd0b4f028a7ce8b26495
Shrija Jha <153726869+shrija-jha@users.noreply.github.com>
2026-06-20 17:45:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ app.use(express.json());
 
 const client = new Client({
     authStrategy: new LocalAuth(),
-    puppeteer: { args: ['--no-sandbox', '--disable-setuid-sandbox'] }
+    puppeteer: { 
+        executablePath: '/usr/bin/chromium', // Tells it to use the Linux system browser
+        args: ['--no-sandbox', '--disable-setuid-sandbox'] 
+    }
 });
 
 client.on('qr', (qr) => {
